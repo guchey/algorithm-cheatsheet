c15705b6574e5ab0e2c43d00d05d9126bc7fa10b
taguchi8s <taguchi8shingen@gmail.com>
2021-02-06 23:01:12 +0900

    add enum_divisors

--- test_integral.py
+++ test_integral.py
@@ -17,6 +17,20 @@ def is_prime(value):
     return True
 
 
+def enum_divisors(value):
+    """
+    約数列挙
+    """
+    result = set()
+    i = 1
+    while i * i <= value:
+        if value % i == 0:
+            result.add(i)
+            result.add(value // i)
+        i += 1
+    return sorted(result)
+
+
 class TestIntegral(unittest.TestCase):
 
     def test_is_prime(self):
@@ -27,6 +41,16 @@ class TestIntegral(unittest.TestCase):
         # Assert
         assert is_prime(59) is True
         assert is_prime(57) is False
+
+    def test_enum_divisors(self):
+        # Arrange
+        expected = [1, 2, 4, 5, 10, 20, 25, 50, 100]
+
+        # Act
+        actual = enum_divisors(100)
+
+        # Assert
+        assert actual == expected
 
 
 if __name__ == '__main__':
